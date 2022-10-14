27fc810cd7e32a465513477b7089d63645545239
anaghakarurkar <anaghakarurkar@gmail.com>
2022-10-14 18:31:26 +0100

    Initial project setup done for Weather Care API. This API gives clothing and weather care suggestions.

---
+++ WeatherCareAPI.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace WeatherCareAPI.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
