f175018a621d7dcb0a7080c3779a683b0132dd51
Ija Saporenkova <ija.sha@gmail.com>
2022-10-15 17:55:05 +0100

    Create classes for getting forecast from json: Forecast, ForecastDaily, daily, daily_units ForecastHourly, hourly, hourly_units

---
+++ WeatherCareAPI/Models.Json/Forecast.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherCareAPI.Models.Json
+{
+    public class Forecast
+    {
+        public float latitude { get; set; }
+        public float longitude { get; set; }
+    }
+}
---
+++ WeatherCareAPI/Models.Json/hourly_units.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherCareAPI.Models.Json
+{
+    public class hourly_units
+    {
+
+        public string windspeed_10m { get; set; }
+        public string temperature_2m { get; set; }
+        public string relativehumidity_2m { get; set; }
+
+    }
+}
