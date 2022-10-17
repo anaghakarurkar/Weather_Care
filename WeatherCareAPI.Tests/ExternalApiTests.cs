b31c6071762972f1da81d295cb28bb307fddf31f
Ija Saporenkova <ija.sha@gmail.com>
2022-10-17 09:19:46 +0100

    Add tests for TestImportFromApiDaily

--- WeatherCareAPI.Tests/ExternalApiTests.cs
+++ WeatherCareAPI.Tests/ExternalApiTests.cs
@@ -7,6 +7,12 @@ namespace WeatherCareAPI.Tests
 
     public class ExternalApiTests
     {
+        private DateTime currentDate;
+        [SetUp]
+        public void Setup()
+        {
+            currentDate = DateTime.Now.Date;
+        }
 
         [TestCase("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,weathercode,relativehumidity_2m,windspeed_10m", 52.52)]
         [TestCase("https://api.open-meteo.com/v1/forecast?latitude=56.95&longitude=24.11&hourly=temperature_2m,weathercode,relativehumidity_2m,windspeed_10m", 56.9375)]
@@ -18,12 +24,21 @@ namespace WeatherCareAPI.Tests
         }
 
         [TestCase("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&timezone=GMT&daily=weathercode,temperature_2m_max,temperature_2m_min,windspeed_10m_max,precipitation_sum", 52.52)]
-        [TestCase("https://api.open-meteo.com/v1/forecast?latitude=56.95&longitude=24.11&hourly=temperature_2m,weathercode,relativehumidity_2m,windspeed_10m", 56.9375)]
+        [TestCase("https://api.open-meteo.com/v1/forecast?latitude=56.95&longitude=24.11&timezone=GMT&daily=weathercode,temperature_2m_max,temperature_2m_min,windspeed_10m_max,precipitation_sum", 56.9375)]
 
         public void TestImportFromApiDailyLatitude(string url, double lat)
         {
             var forecast = ImportFromApi.ImportForecastDaily(url).GetAwaiter().GetResult();
             forecast.latitude.Should().Be(lat);
+        }
+
+        [TestCase("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&timezone=GMT&daily=weathercode,temperature_2m_max,temperature_2m_min,windspeed_10m_max,precipitation_sum")]
+
+        public void TestImportFromApiDailyCurrentDate(string url)
+        {
+
+            var forecast = ImportFromApi.ImportForecastDaily(url).GetAwaiter().GetResult();
+            forecast.daily.time[0].Should().Be(currentDate);
         }
 
 
