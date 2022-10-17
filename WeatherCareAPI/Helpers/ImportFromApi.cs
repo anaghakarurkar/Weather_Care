d93222f0279cf5a5925b62fd3cb9711dc4e1473c
Ija Saporenkova <ija.sha@gmail.com>
2022-10-17 09:08:35 +0100

    Add method ImportForecastDaily

--- WeatherCareAPI/Helpers/ImportFromApi.cs
+++ WeatherCareAPI/Helpers/ImportFromApi.cs
@@ -7,10 +7,16 @@ namespace WeatherCareAPI.Helpers
 {
     public class ImportFromApi
     {
-        //static async System.Threading.Tasks.Task ForecastDaily ExportForecastDaily(string url)
-        //{
-
-        //}
+        public static async Task<ForecastDaily> ImportForecastDaily(string url)
+        {
+            //url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&timezone=GMT&daily=weathercode,temperature_2m_max,temperature_2m_min,windspeed_10m_max,precipitation_sum";
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<ForecastDaily>(responseBody);
+            return result;
+        }
 
         public static async Task<ForecastHourly> ImportForecastHourly(string url)
         {
