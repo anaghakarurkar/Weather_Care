afdfcdc4d0a59de6032b4d82f130e4bf2664aabc
Dennis Wong <cfwwong@gmail.com>
2022-10-22 10:43:12 +0100

    Refactored validation of getlocation input.

--- WeatherCareAPI/Helpers/Utilities.cs
+++ WeatherCareAPI/Helpers/Utilities.cs
@@ -3,9 +3,9 @@ namespace WeatherCareAPI.Helpers
 {
     public class Utilities
     {
-        public static string errorMsg(int type, string cityName)
+        public static string errorMsg(string type, string cityName)
         {
-            if (type == 1)
+            if (type == "cityNotFound")
                 return $"City name \"{cityName}\" doesn't exist in our database, please use geolocation instead.";
             else
                 return "Incorrect geolocation, please input -90 to +90 for latitude, and -180 to +180 for longitude.";
